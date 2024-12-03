e1885b87ebe20c06e1adac838b4895f386a64372
Mahadevan Reji <rmahadevan574@gmail.com>
2024-12-04 00:02:17 +0530

    test

--- connectToDb.java
+++ connectToDb.java
@@ -1,16 +1,10 @@
 import java.sql.*;
 
-//pass :
-//user: avnadmin
-
-//uri : mysql://avnadmin:AVNS_NXzsmswZAd0COxYvhmb@mysql-1bfd5082-rmahadevan573-5ab3.e.aivencloud.com:13235/defaultdb?ssl-mode=REQUIRED
-
 public class connectToDb {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String password = System.getenv("AIVEN_SERVICE_PASSWORD"); // or use a secure vault
-            Connection con = DriverManager.getConnection("jdbc:mysql://mysql-1bfd5082-rmahadevan573-5ab3.e.aivencloud.com:13235/defaultdb?ssl-mode=REQUIRED", "avnadmin", password);
+            Connection con = DriverManager.getConnection("url", "username","password");
             Statement stmt = con.createStatement();
             
             //create table
